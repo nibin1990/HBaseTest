6bed0fc6ea4a842dcacf18335c7892b636ed2c9b
Nibin Abraham Mattam <nibin@nibins-mbp.gateway.pace.com>
2015-08-02 14:55:35 -0500

    Initial Commit

---
+++ src/main/java/com/nibin/hbase/App.java
@@ -0,0 +1,13 @@
+package com.nibin.hbase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
